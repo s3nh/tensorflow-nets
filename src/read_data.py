5db32581c467f8c529cad854ebd47225a0b4bb20
s3nh <steam.panek@gmail.com>
2020-01-05 10:47:48 +0100

    Add data preprocess

--- src/read_data.py
+++ src/read_data.py
@@ -4,12 +4,12 @@ import os
 import pathlib 
 import matplotlib.pyplot as plt 
 import numpy as np 
-
+from  src.utils import _get_data_info
 # Set precision 
 
-
-DATA_PATH = 'data'
+DATA_PATH = 'data/'
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+
 
 
 def data_gen(path):
@@ -20,9 +20,9 @@ def data_gen(path):
     _files = tf.data.Dataset.list_files(os.path.join(path, '*/*'))
     return _files
 
-def get_label(file_path):
+def get_label(file_path, class_names):
     parts = tf.strings.split(file_path, os.path.sep)
-    return parts[-2] == CLASS_NAMES
+    return parts[-2] == class_names
 
 # Image decoding 
 
@@ -34,26 +34,23 @@ def decode_img(img):
 # Preprocess path 
 
 def process_path(file_path):
-    label = get_label(file_path)
+    image_count, CLASS_NAMES = _get_data_info(file_path)
+    label = get_label(file_path, CLASS_NAMES)
     img = tf.io.read_file(file_path)
     img = decode_img(img)
     return img, label
 
-
-def get_structured_dataset(file_path):
-    labeled_ds =  file_path.map(process_path, num_parallel_calls = AUTOTUNE)
+def get_structured_dataset(listed_files):
+    labeled_ds =  listed_files.map(process_path, num_parallel_calls = AUTOTUNE)
     return labeled_ds
-
-
 
 
 def main():
     files = data_gen(path = DATA_PATH)
-    for file in files.take(5):
-        print(file.numpy())
-    labels = get_label(files)
-    print(labels)
-
+    CLASS_NAMES, image_count = _get_data_info(DATA_PATH)
+    print("\n".format([el for el in CLASS_NAMES]))
+    labeled_ds = get_structured_dataset(files)
+    print(labeled_ds)
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,29 @@
+import numpy as np 
 import tensorflow as tf
-
+import pathlib
 """
 Utility functions 
 preparation for training 
 
 """
+
+
+def _get_data_info(data_dir, ff = '.jpg'):
+    """
+    Return information about 
+    data and its labels 
+    Args:
+        data_dir:
+            data_dir to preprocess 
+        ff: 
+            format of preprocess files 
+    """
+    data_dir = pathlib.Path(data_dir)
+    image_count = len(list(data_dir.glob('*/*{}'.format(ff))))
+    #data_dir = tf.data.Dataset.list_files(str(data_dir/'*/*')) 
+    assert image_count > 0, 'Indicated path is empty'
+    CLASS_NAMES =  np.array([item.name for item in data_dir.glob('*')])
+    return CLASS_NAMES, image_count 
 
 
 def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
@@ -20,3 +39,4 @@ def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
     ds = ds.batch(BATCH_SIZE)
     ds = ds.prefetch(buffer_size = AUTOTUNE)
     return ds
+
