af68d87344adce9676f30fc7b618e8366992285d
s3nh <steam.panek@gmail.com>
2020-01-06 11:09:51 +0100

    Add training phase

--- models/models.py
+++ models/models.py
@@ -14,7 +14,6 @@ def load_model(IMG_SHAPE= (224, 224, 3)):
    base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE, 
    include_top = False, 
    weights = 'imagenet') 
-   print(base_model.summary())
    base_model.trainable = False 
    global_average_layer = _classification_head()
    prediction_layer = _prediction_layer()
